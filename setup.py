7fd3a884399e259dad64cb7ff1893bcf1e5e596a
Hanno Schlichting <hanno@hannosch.eu>
2008-03-27 15:05:18 +0000

    Bump version after 1.6 release
    svn path=/buildout/plone.recipe.zope2instance/trunk/; revision=61402

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2instance"
-version = '1.6'
+version = '1.7'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
