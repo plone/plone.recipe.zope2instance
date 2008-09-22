3d31a303f56de6fc591c69f02fa75b439011814b
Wichert Akkerman <wichert@wiggy.net>
2008-09-22 18:12:57 +0000

    Tag 2.5
    svn path=/buildout/plone.recipe.zope2instance/trunk/; revision=72328

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2instance"
-version = '2.5'
+version = '2.6'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
