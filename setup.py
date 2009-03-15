4e98de0ff2bc3c9ae0e62ef2c21b90675838f1d2
Steve McMahon <steve@reidmcmahon.com>
2009-03-15 21:42:27 +0000

    trunk is now 3.2
    svn path=/buildout/plone.recipe.zope2instance/trunk/; revision=82425

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2instance"
-version = '3.1'
+version = '3.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
