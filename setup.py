9d5b31cb3a3f7a4b86211671bda3c62c617f1990
Hanno Schlichting <hanno@hannosch.eu>
2012-09-20 15:27:09 +0200

    pep8

--- setup.py
+++ setup.py
@@ -3,36 +3,37 @@ from setuptools import setup, find_packages
 name = "plone.recipe.zope2instance"
 version = '4.2.5.dev0'
 
+
 def read(name):
     return open(name).read()
 
-long_description=(
-        read('README.rst')
-        + '\n' +
-        read('CHANGES.txt')
-    )
+long_description = (
+    read('README.rst')
+    + '\n' +
+    read('CHANGES.txt')
+)
 
 setup(
-    name = name,
-    version = version,
-    author = "Hanno Schlichting",
-    author_email = "hanno@hannosch.eu",
-    description = "Buildout recipe for creating a Zope 2 instance",
+    name=name,
+    version=version,
+    author="Hanno Schlichting",
+    author_email="hanno@hannosch.eu",
+    description="Buildout recipe for creating a Zope 2 instance",
     long_description=long_description,
-    license = "ZPL 2.1",
-    keywords = "zope2 buildout",
+    license="ZPL 2.1",
+    keywords="zope2 buildout",
     url='http://pypi.python.org/pypi/plone.recipe.zope2instance',
     classifiers=[
-      "License :: OSI Approved :: Zope Public License",
-      "Framework :: Buildout",
-      "Framework :: Plone",
-      "Framework :: Zope2",
-      ],
-    packages = find_packages('src'),
-    include_package_data = True,
-    package_dir = {'':'src'},
-    namespace_packages = ['plone', 'plone.recipe'],
-    install_requires = [
+        "License :: OSI Approved :: Zope Public License",
+        "Framework :: Buildout",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+    ],
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir={'': 'src'},
+    namespace_packages=['plone', 'plone.recipe'],
+    install_requires=[
         'zc.buildout',
         'setuptools',
         'mailinglogger',
@@ -41,5 +42,5 @@ setup(
         'ZODB3 >= 3.9',
     ],
     zip_safe=False,
-    entry_points = {'zc.buildout': ['default = %s:Recipe' % name]},
-    )
+    entry_points={'zc.buildout': ['default = %s:Recipe' % name]},
+)
