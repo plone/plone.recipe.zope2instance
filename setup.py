4dc6ae00fbaea7b80c98a306c7d411a01afb8e06
Gil Forcada Codinachs <gil.gnome@gmail.com>
2016-10-23 10:04:03 -0400

    One import per line

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
+
 
 name = "plone.recipe.zope2instance"
 version = '4.2.23.dev0'
