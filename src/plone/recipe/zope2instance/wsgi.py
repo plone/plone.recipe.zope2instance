99fdfa18e4b45a75f5bc5b90b9d19b589bc1de2d
Thomas Schorr <t_schorr@gmx.de>
2017-02-03 13:21:03 +0000

    remove breakpoint

--- src/plone/recipe/zope2instance/wsgi.py
+++ src/plone/recipe/zope2instance/wsgi.py
@@ -2,7 +2,6 @@ def waitress_main(args=None):
     from paste.deploy import loadserver
     from waitress import serve
     from Zope2.Startup.run import make_wsgi_app
-    import pdb; pdb.set_trace()
     wsgiapp = make_wsgi_app(
         {'debug_mode': 'on'},
         '/home/vagrant/plone5devel/parts/wsgi-instance/etc/zope.conf')
