bdcbd45f5c851059cc681684902ba07ec18acfd1
esteele <eric@esteele.net>
2018-11-08 14:14:38 +0900

    Preparing release 6.0.0
    [ci skip]

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-6.0.0 (unreleased)
+6.0.0 (2018-11-08)
 ------------------
 
 Breaking changes:
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 
 name = "plone.recipe.zope2instance"
-version = '5.0.2.dev0'
+version = '6.0.0'
 
 setup(
     name=name,
