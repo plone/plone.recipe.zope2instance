2c98d2d9e3985e95c68d42cd2d695f16d3cc256e
tschorr <t_schorr@gmx.de>
2019-12-17 14:36:35 +0100

    simplify

--- src/plone/recipe/zope2instance/sentry.py
+++ src/plone/recipe/zope2instance/sentry.py
@@ -9,7 +9,6 @@ from sentry_sdk.integrations.logging import (
     DEFAULT_EVENT_LEVEL,
     EventHandler,
     _can_record,
-    _breadcrumb_from_record,
     _logging_to_event_level,
     _extra_from_record)
 
@@ -22,14 +21,7 @@ from sentry_sdk.utils import (
 
 class ZopeEventHandler(EventHandler):
 
-    def emit(self, record):
-        # type: (LogRecord) -> Any
-        with capture_internal_exceptions():
-            self.format(record)
-            return self._emit(record)
-
     def _emit(self, record):
-        # type: (LogRecord) -> None
         if not _can_record(record):
             return
 
@@ -70,10 +62,13 @@ class ZopeEventHandler(EventHandler):
 
         event["level"] = _logging_to_event_level(record.levelname)
         event["logger"] = record.name
-        event["logentry"] = {"message": to_string(record.msg), "params": record.args}
+        event["logentry"] = {
+            "message": to_string(record.msg),
+            "params": record.args}
         event["extra"] = _extra_from_record(record)
 
         hub.capture_event(event, hint=hint)
+
 
 class ZopeSentryLogging(LoggingIntegration):
 
